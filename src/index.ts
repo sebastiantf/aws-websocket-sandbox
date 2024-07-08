12af8cbd7223f43bd1245fa2d64d24033f5d044d
Sebastian T F <sebastian.tharakan97@gmail.com>
2024-07-08 17:28:50 +0530

    feat: add ws client

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
+import WebSocket from 'ws';
+import dotenv from 'dotenv';
 import { logger } from './common/logger';
-import { Greeter } from './lib/greeter';
+dotenv.config();
 
-logger.info(Greeter('Joe'));
+const ws = new WebSocket(process.env.WSS_URL || '', {
+  headers: {
+    Auth: process.env.WSS_AUTH,
+  },
+});
+
+ws.on('error', console.error);
+
+ws.on('open', function open() {
+  logger.info('connected');
+  logger.debug('sending ping');
+  ws.ping();
+  logger.debug('ping sent');
+});
+
+ws.on('message', function message(data) {
+  logger.info('received: %s', data);
+});
+
+ws.on('pong', function pong() {
+  logger.debug('received pong');
+});
